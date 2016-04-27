b2cada3611369e0cd096713636d931f3f6835d83
flozada2 <flozada@costco.com>
2016-04-27 10:03:34 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,71 +1,33 @@
-## site.pp ##
-
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
-
-## Active Configurations ##
-
-# PRIMARY FILEBUCKET
-# This configures puppet agent and puppet inspect to back up file contents when
-# they run. The Puppet Enterprise console needs this to display file contents
-# and differences.
-
-# Disable filebucket by default for all File resources:
-File { backup => false }
-
-# Randomize enforcement order to help understand relationships
-ini_setting { 'random ordering':
-  ensure  => present,
-  path    => "${settings::confdir}/puppet.conf",
-  section => 'agent',
-  setting => 'ordering',
-  value   => 'title-hash',
+class nginx {
+File {
+owner => 'root',
+group => 'root',
+mode => '0664',
 }
-
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  notify { 'hostmessage':
-    message => "Hello World! This node's name is ${::hostname}",
-  }
-
-  include memcached
-  include nginx
-
-  #file { '/etc/motd':
-  #  ensure  => file,
-  #  owner   => 'root',
-  #  group   => 'root',
-  #  mode    => '0644',
-  #  content => "Hey hey, we're the MONKEYS\n",
-  #}
-
-  #exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-  #  path    => '/usr/local/bin',
-  #  creates => '/etc/motd',
-  #}
-
-  #host { 'testing.puppetlabs.vm':
-  #  ensure => present,
-  #  ip     => '127.0.0.1',
-  #}
-
-
+package { 'nginx':
+ensure => present,
+}
+file { [ '/var/www', '/etc/nginx/conf.d' ]:
+ensure => directory,
+}
+file { '/var/www/index.html':
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+file { '/etc/nginx/nginx.conf':
+ensure => file,
+source => 'puppet:///modules/nginx/nginx.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+file { '/etc/nginx/conf.d/default.conf':
+ensure => file,
+source => 'puppet:///modules/nginx/default.conf',
+notify => Service['nginx'],
+require => Package['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
 }
