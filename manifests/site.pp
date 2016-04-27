10016a00801cc45f69e7df6fe6710c004927b3ac
flozada2 <flozada@costco.com>
2016-04-27 11:14:51 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,33 +1,9 @@
-class nginx {
-File {
-owner => 'root',
-group => 'root',
-mode => '0664',
-}
-package { 'nginx':
-ensure => present,
-}
-file { [ '/var/www', '/etc/nginx/conf.d' ]:
-ensure => directory,
-}
-file { '/var/www/index.html':
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
-}
-file { '/etc/nginx/nginx.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/nginx.conf',
-require => Package['nginx'],
-notify => Service['nginx'],
-}
-file { '/etc/nginx/conf.d/default.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/default.conf',
-notify => Service['nginx'],
-require => Package['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
+node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
 }
 }
