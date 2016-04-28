102dc1e644881c3fcddd36611de154eb47825d62
flozada2 <flozada@costco.com>
2016-04-28 10:48:29 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,69 +1,82 @@
+#
 class nginx (
-$root = undef,
+  $root = undef,
 ) {
-case $::osfamily {
-'redhat','debian' : {
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-# $docroot = '/var/www'
-$confdir = '/etc/nginx'
-$logdir = '/var/log/nginx'
-# this will be used if we don't pass in a value
-$default_docroot = '/var/www'
-}
-'windows' : {
-$package = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-# $docroot = 'C:/ProgramData/nginx/html'
-$confdir = 'C:/ProgramData/nginx'
-$logdir = 'C:/ProgramData/nginx/logs'
-# this will be used if we don't pass in a value
-$default_docroot = 'C:/ProgramData/nginx/html'
-}
-default : {
-fail("Module ${module_name} is not supported on ${::osfamily}")
-}
-}
-# user the service will run as. Used in the nginx.conf.erb template
-$user = $::osfamily
-'redhat' => 'nginx',
-'debian' => 'www-data',
-'windows' => 'nobody',
-}
-# if $root isn't set, then fall back to the platform default
-$docroot = $root ? {
-undef => $default_docroot,
-default => $root,
-}
-File {
-owner => $owner,
-group => $group,
-mode => '0664',
-}
-package { $package:
-ensure => present,
-}
-file { [ $docroot, "${confdir}/conf.d" ]:
-ensure => directory,
-}
-file { "${docroot}/index.html":
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
-}
-file { "${confdir}/nginx.conf":
-ensure => file,
-content => template('nginx/nginx.conf.erb'),
-notify => Service['nginx'],
-}
-file { "${confdir}/conf.d/default.conf":
-ensure => file,
-content => template('nginx/default.conf.erb'),
-notify => Service['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
-}
+
+  case $::osfamily {
+    'redhat','debian': {
+      $package         = 'nginx'
+      $owner           = 'root'
+      $group           = 'root'
+      $default_docroot = '/var/www'
+      $confdir         = '/etc/nginx'
+      $logdir          = '/var/log/nginx'
+    }
+    'windows': {
+      $package         = 'nginx-service'
+      $owner           = 'Administrator'
+      $group           = 'Administrators'
+      $default_docroot = 'c:/ProgramData/nginx/html'
+      $confdir         = 'c:/ProgramData/nginx'
+      $logdir          = 'c:/ProgramData/nginx/logs'
+    }
+    default: {
+      fail("${::osfamily} is not supported by this module")
+    }
+  }
+  $svc = 'nginx'
+  $source_base = "puppet:///modules/${module_name}"
+  $nginx_user = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
+  }
+  $docroot = $root ? {
+    undef   => $default_docroot,
+    default => $root,
+  }
+
+  File {
+    ensure => file,
+    owner  => $owner,
+    group  => $group,
+    mode   => '0644',
+  }
+
+  # PACKAGE
+  package { $package:
+    ensure => present,
+  }
+
+  # DOCROOT
+  file { $docroot:
+    ensure => directory,
+  }
+
+  # WEB PAGE
+  file { "${docroot}/index.html":
+    source => "${source_base}/index.html",
+  }
+
+  # CONFIG FILES
+  file { "${confdir}/nginx.conf":
+    #source  => "${source_base}/nginx.conf",
+    content => template('nginx/nginx.conf.erb'),
+    require => Package[$package],
+    notify  => Service[$svc],
+  }
+
+  file { "${confdir}/conf.d/default.conf":
+    #source  => "${source_base}/default.conf",
+    content => template('nginx/default.conf.erb'),
+    require => Package[$package],
+    notify  => Service[$svc],
+  }
+
+  # SERVICE
+  service { $svc:
+    ensure => running,
+    enable => true,
+  }
+
 }
